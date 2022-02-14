4855d2bf1007db01c84409cc5cae5e3e6c79d25c
Aaron Toliver <flux.charlotte@gmail.com>
2022-02-14 17:22:02 -0500

    shadows added

--- pages/components/Button.js
+++ pages/components/Button.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Button = () => {
   return (
-        <button className="bg-primary-light text-white font bold py-2 px-4 rounded md:text-2xl md:py-4 md:px-6">
+        <button className="bg-primary-light text-white font bold py-2 px-4 rounded md:text-2xl md:py-4 md:px-6 box-shadow">
             Contact
         </button>
   )
