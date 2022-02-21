468114da9375325e8f06b4a4fe18e5b5c5b76908
Aaron Toliver <flux.charlotte@gmail.com>
2022-02-21 16:25:56 -0500

    full nav started

---
+++ pages/components/Nav.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import FullNav from './FullNav'
+
+const Nav = () => {
+  return (
+    <div>
+      <FullNav />
+    </div>
+  )
+}
+
+export default Nav
--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Image from 'next/image'
 
 //components
 import styles from '../styles/Home.module.css'
+import Nav from './components/Nav'
 import Hero from './components/Hero'
 import Services from './components/Services'
 import References from './components/References'
@@ -10,9 +11,11 @@ import Form from './components/Form'
 import Footer from './components/Footer'
 import Gallery from './components/Gallery'
 
+
 export default function Home() {
   return (
     <div>
+      <Nav />
       <Hero/>           
       <Services />
       <Gallery /> 
