d1425e7edcc7ca9a4b4fdd75956729cb195671c2
Aaron Toliver <flux.charlotte@gmail.com>
2022-02-22 14:57:54 -0500

    href solution attempt

--- pages/components/Button.js
+++ pages/components/Button.js
@@ -6,7 +6,7 @@ const Button = (props) => {
 
   return (
 
-    <Link href={href} >
+    <Link href={href ?? ''} >
         <button className="bg-primary-dark uppercase font-semibold text-white font bold py-2 px-4 rounded md:text-lg md:py-2 md:px-6 box-shadow hover:bg-yellow-300 hover:text-blue-900 transition-all">          
           <a>{name}</a>
         </button>
