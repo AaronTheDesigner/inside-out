e5f5acd06433f764bd8c6633954217274f1744cb
Aaron Toliver <flux.charlotte@gmail.com>
2022-02-23 16:02:08 -0500

    mobile nav started

--- pages/components/Nav.js
+++ pages/components/Nav.js
@@ -1,10 +1,18 @@
 import React from 'react'
 import FullNav from './FullNav'
+import MobileNav from './MobileNav'
+import { useState } from 'react'
 
 const Nav = () => {
+
+
+  const renderNav = () => {
+
+  }
   return (
     <div>
       <FullNav />
+      <MobileNav />
     </div>
   )
 }
