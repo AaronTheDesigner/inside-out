3eb0dd80439da397c5bfbf15df8dc6743fda1eb0
Aaron Toliver <flux.charlotte@gmail.com>
2022-03-03 14:13:03 -0500

    form

--- pages/components/Form.js
+++ pages/components/Form.js
@@ -30,59 +30,11 @@ const Form = () => {
         </h1>
         <div className="p-3 sm:p-10 md:p-15 lg:p-20 border-2 border-blue-900 rounded-lg">
             <form action="" className='flex flex-col gap-3' onSubmit={handleSubmit}>
-                <label htmlFor="" className="block">
-                    <span className="text-gray-700">Full Name</span>
-                    <input type="text" className="form-input mt-1 block w-full" placeholder='Full Name'/>
-                </label>
-                <label htmlFor="" className="block">
-                    <span className="text-gray-700">Email</span>
-                    <input type="email" className="form-input mt-1 block w-full" placeholder='Email'/>
-                </label>
-                <label htmlFor="" className="block">
-                    <span className="text-gray-700">Services Requested</span>
-                    <div className="mt-2">
-                        <div>
-                            <label htmlFor="" className="inline-flex items-center">
-                                <input type="checkbox" className="form-checkbox" />
-                                <span className="ml-2">Option 1</span>
-                            </label>
-                        </div>
-                        <div>
-                            <label htmlFor="" className="inline-flex items-center">
-                                <input type="checkbox" className="form-checkbox" />
-                                <span className="ml-2">Option 2</span>
-                            </label>
-                        </div>
-                        <div>
-                            <label htmlFor="" className="inline-flex items-center">
-                                <input type="checkbox" className="form-checkbox" />
-                                <span className="ml-2">Option 3</span>
-                            </label>
-                        </div>
-                        <div>
-                            <label htmlFor="" className="inline-flex items-center">
-                                <input type="checkbox" className="form-checkbox" />
-                                <span className="ml-2">Option 4</span>
-                            </label>
-                        </div>
-                        <div>
-                            <label htmlFor="" className="inline-flex items-center">
-                                <input type="checkbox" className="form-checkbox" />
-                                <span className="ml-2">Option 5</span>
-                            </label>
-                        </div>
-                    </div>
-                </label>
-                <label htmlFor="" className="block">
-                    <span className="text-gray-700">Message</span>
-                    <textarea name="" id="" cols="" rows="3" className="form-textarea mt-1 block w-full h-24" placeholder='Message'></textarea>
-                </label>
-
-                
-                    <button>
-                        Submit
-                    </button>
-                
+                <label htmlFor="name">Full Name</label>
+                <input type="text" name="name" id="name" />
+                <button>
+                    Submit    
+                </button>                
 
             </form>
         </div>
